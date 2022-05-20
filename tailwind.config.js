7cffcf13a95312a932da0417fda69646895f9bdc
Thanh <nmthanh20006@gmail.com>
2022-05-20 15:13:12 +0700

    set up tailiwnd

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: "jit",
+    purge: [
+       "./src/pages/**/*.{js,ts,jsx,tsx}",
+       "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: false, 
+    theme: {
+
+       extend: {},
+    },
+    variants: {
+       extend: {},
+    },
+    plugins: [],
+ };
+ 
